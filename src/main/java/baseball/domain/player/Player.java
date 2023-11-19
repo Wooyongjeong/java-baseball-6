4834ff5ab3617afc9024987a4877b87075855189
wooyongjeong <wooyong.dev@gmail.com>
2023-11-19 21:02:07 +0900

    feat(player): create Player skeleton code

---
+++ src/main/java/baseball/domain/player/Player.java
@@ -0,0 +1,11 @@
+package baseball.domain.player;
+
+import baseball.domain.Baseball;
+
+public class Player {
+    private final Baseball baseball;
+
+    public Player(Baseball baseball) {
+        this.baseball = baseball;
+    }
+}
