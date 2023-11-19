7509c14ad6a30f11cc7a4f23bdb7b082ca66ef14
wooyongjeong <wooyong.dev@gmail.com>
2023-11-19 21:01:52 +0900

    feat(computer): create Computer skeleton code

---
+++ src/main/java/baseball/domain/computer/Computer.java
@@ -0,0 +1,11 @@
+package baseball.domain.computer;
+
+import baseball.domain.Baseball;
+
+public class Computer {
+    private final Baseball baseball;
+
+    public Computer(Baseball baseball) {
+        this.baseball = baseball;
+    }
+}
