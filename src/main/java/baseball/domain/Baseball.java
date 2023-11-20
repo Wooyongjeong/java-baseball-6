3f992473ce15026ec52befd84ac4bd3bd7026eef
wooyongjeong <wooyong.dev@gmail.com>
2023-11-20 23:02:57 +0900

    feat(baseball): Baseball 예외 처리 구현
    - 3자리 숫자가 아닌 경우
    - 중복된 숫자가 있는 경우
    - 숫자 중 '0'이 포함된 경우

--- src/main/java/baseball/domain/Baseball.java
+++ src/main/java/baseball/domain/Baseball.java
@@ -5,25 +5,31 @@ import java.util.List;
 import java.util.Set;
 
 public class Baseball {
-    private static final int SIZE = 3;
+    public static final int SIZE = 3;
+    public static final int MIN_NUM = 1;
+    public static final int MAX_NUM = 9;
     private final List<Integer> numbers;
 
-    public Baseball(List<Integer> numbers) {
+    private Baseball(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+    }
+
+    public static Baseball from(List<Integer> numbers) {
+        return new Baseball(numbers);
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != SIZE) {
             throw new IllegalBaseballNumbersException();
         }
-        validateDuplicate(numbers);
-    }
-
-    private void validateDuplicate(List<Integer> numbers) {
         Set<Integer> set = Set.copyOf(numbers);
         if (set.size() != SIZE) {
             throw new IllegalBaseballNumbersException();
         }
+        if (numbers.contains(0)) {
+            throw new IllegalBaseballNumbersException();
+        }
+    }
     }
 }
